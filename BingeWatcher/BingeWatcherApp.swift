b0ce9aa7b219531c6917c50c93a8ee1c48b8a4a2
Sharath Chenna <sharathchenna87@gmail.com>
2026-03-18 00:24:32 +0530

    Initial Commit

---
+++ BingeWatcher/BingeWatcherApp.swift
@@ -0,0 +1,32 @@
+//
+//  BingeWatcherApp.swift
+//  BingeWatcher
+//
+//  Created by Sharath Chenna on 18/03/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct BingeWatcherApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
